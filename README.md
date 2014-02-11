c9445170e0489b00f1400c3a4825453b22b8c0b7
scherrma <ersatz.edenite@gmail.com>
2014-02-10 21:42:02 -0500

    recorded timings in the readme

--- README.md
+++ README.md
@@ -2,3 +2,51 @@ cse491-parallel
 ===============
 CSE 491 section 4, homework 2
 By Ben Taylor and Marty Scherr
+
+timings:
+10 core 2p machine
+
+
+rand:
+Single threaded took 4344ms (4.34452s)
+1 threads took 4758ms (4.75834s)
+Speedup: 0.913032    (fraction of linear: 0.913032)
+
+10 threads took 1171ms (1.17173s)
+Speedup: 3.70777    (fraction of linear: 0.370777)
+
+20 threads took 952ms (0.952221s)
+Speedup: 4.56251    (fraction of linear: 0.228125)
+
+30 threads took 976ms (0.976392s)
+Speedup: 4.44956    (fraction of linear: 0.148319)
+
+
+mult:
+Single threaded took 974ms (0.974285s)
+1 threads took 982ms (0.982585s)
+Speedup: 0.991553    (fraction of linear: 0.991553)
+
+10 threads took 118ms (0.118999s)
+Speedup: 8.18734    (fraction of linear: 0.818734)
+
+20 threads took 69ms (0.069368s)
+Speedup: 14.0452    (fraction of linear: 0.702258)
+
+30 threads took 69ms (0.06951s)
+Speedup: 14.0165    (fraction of linear: 0.467216)
+
+
+add:
+Single threaded took 1120ms (1.1205s)
+1 threads took 1103ms (1.10327s)
+Speedup: 1.01562    (fraction of linear: 1.01562)
+
+10 threads took 1008ms (1.00849s)
+Speedup: 1.11107    (fraction of linear: 0.111107)
+
+20 threads took 1010ms (1.01038s)
+Speedup: 1.10899    (fraction of linear: 0.0554496)
+
+30 threads took 1009ms (1.00986s)
+Speedup: 1.10957    (fraction of linear: 0.0369855)
--- main.cpp
+++ main.cpp
@@ -6,38 +6,45 @@ using std::chrono::milliseconds;
 using std::endl;
 using std::move;
 
-void tBigAdd(int t) {
+void bigAdd(int trand) {
   constexpr int sz = 10000;
   using T = double;
   Matrix<T> d1(sz), d2(sz), d3(sz);
-  d1.tRand(0, 1, 13, t);
-  d2.tRand(0, 1, 16, t);
-  d1.tAdd(d2, d3, t);
-}
-
-void bigAdd() {
-  constexpr int sz = 10000;
-  constexpr int t = 16;
-  using T = double;
-  Matrix<T> d1(sz), d2(sz), d3(sz);
-  d1.tRand(0, 1, 13, t);
-  d2.tRand(0, 1, 16, t);
+  d1.tRand(0, 1, 13, trand);
+  d2.tRand(0, 1, 16, trand);
   d1.add(d2, d3);
 }
 
-void tBigMult(int t) {
+void tBigAdd(int tadd, int trand) {
+  constexpr int sz = 10000;
+  using T = double;
+  Matrix<T> d1(sz), d2(sz), d3(sz);
+  d1.tRand(0, 1, 13, trand);
+  d2.tRand(0, 1, 16, trand);
+  d1.tAdd(d2, d3, tadd);
+}
+void bigMult(int trand) {
   constexpr int sz = 1000;
   using T = double;
   Matrix<T> d1(sz), d2(sz), d3(sz);
-  d1.tRand(0, 1, 13, t);
-  d2.tRand(0, 1, 16, t);
-  d1.tMult(d2, d3, t);
+  d1.tRand(0, 1, 13, trand);
+  d2.tRand(0, 1, 16, trand);
+  d1.mult(d2, d3);
+}
+
+void tBigMult(int tmult, int trand) {
+  constexpr int sz = 1000;
+  using T = double;
+  Matrix<T> d1(sz), d2(sz), d3(sz);
+  d1.tRand(0, 1, 13, trand);
+  d2.tRand(0, 1, 16, trand);
+  d1.tMult(d2, d3, tmult);
 }
 
 
-void randFill() {
+void bigRand() {
   using T = double;
-  constexpr int sz = 1000;
+  constexpr int sz = 10000;
   Matrix<T> d1(sz), d2(sz), d3(sz);
   d1.rand(0,1,10);
   d2.rand(0,1,12);
@@ -46,7 +53,7 @@ void randFill() {
 
 void tBigRand(int t) {
   using T = double;
-  constexpr int sz = 1000;
+  constexpr int sz = 10000;
   Matrix<T> d1(sz), d2(sz), d3(sz);
   d1.tRand(0,1,10,t);
   d2.tRand(0,1,12,t);
@@ -56,7 +63,7 @@ void tBigRand(int t) {
 int main(int argc, char const *argv[]) {
   (void)argc;
   (void)argv;
-
+/*
   // Test constructors
   Matrix<int64_t> a(4);
   Matrix<int64_t> b(4, 7);
@@ -112,66 +119,52 @@ int main(int argc, char const *argv[]) {
   Matrix<int64_t> m(4, 0);
   m.rand(0, 10);
   cout << "m: " << endl << m << endl;
-
-  microseconds d1{ 0 }, d2{ 0 };
-
-  /*for (int i = 0; i < 10; i++) {
-    auto t1 = hrc::now();
-    bigAdd(); 
-    // randFill();
-    auto t2 = hrc::now();
-
-    dd += duration_cast<microseconds>(t2 - t1);
-  }
-
-  dd /= 10;
-
-  cout << "Took " << duration_cast<milliseconds>(dd).count() << "ms ("
-       << static_cast<double>(dd.count()) / 1000000 << "s)" << endl;
-
-  dd -= dd;
 */
-  
+
   //parallelism testing
+  microseconds d1{ 0 }, d2{ 0 };
   int threadcount = 16;
- /* constexpr int sz = 100;
+  constexpr int sz = 100;
   using T = double;
   Matrix<T> m1(sz), m2(sz), m3(sz);
   m1.tRand(0, 1, 13, threadcount);
   m2.tRand(0, 1, 16, threadcount);
-  m1.tMult(m2, m3, threadcount);
-  cout << "correctness test " << (m3 == mult<T>(m1,m2) ? "passed" : "failed") << endl;
-*/
+  m1.tAdd(m2, m3, threadcount);
+  //cout << "correctness test " << (m3 == add<T>(m1,m2) ? "passed" : "failed") << endl;
 
   //single thread timing
   for (int i = 0; i < 10; i++) {
     auto t1 = hrc::now();
-    tBigRand(1);
+    bigAdd(threadcount);
     auto t2 = hrc::now();
 
     d1 += duration_cast<microseconds>(t2 - t1);
   }
   d1 /= 10;
 
-  //multithread timing
-  for (int i = 0; i < 10; i++) {
-    auto t1 = hrc::now();
-    tBigRand(threadcount);
-    auto t2 = hrc::now();
+  cout << "Single threaded took " << duration_cast<milliseconds>(d1).count() << "ms ("
+       << static_cast<double>(d1.count()) / 1000000 << "s)" << endl;
 
-    d2 += duration_cast<microseconds>(t2 - t1);
+  for(auto tnum : vector<int>{1,10,20,30}) {
+    d2 *= 0;
+    //multithread timing
+    for (int i = 0; i < 10; i++) {
+      auto t1 = hrc::now();
+      tBigAdd(tnum,threadcount);
+      auto t2 = hrc::now();
+
+      d2 += duration_cast<microseconds>(t2 - t1);
+    }
+    d2 /= 10;
+
+    //parallelism outputs
+    cout << tnum << " threads took " << duration_cast<milliseconds>(d2).count() << "ms ("
+         << static_cast<double>(d2.count()) / 1000000 << "s)" << endl
+         << "Speedup: " << 1.0*d1/d2 
+         << "    (fraction of linear: " << 1.0*d1/(d2*tnum) << ")" << endl << endl;
   }
-  d2 /= 10;
 
-  //parallelism outputs
-  cout << "1 thread took " << duration_cast<milliseconds>(d1).count() << "ms ("
-       << static_cast<double>(d1.count()) / 1000000 << "s)" << endl
-       << threadcount << " threads took " << duration_cast<milliseconds>(d2).count() << "ms ("
-       << static_cast<double>(d2.count()) / 1000000 << "s)" << endl
-       << "Speedup: " << 1.0*d1/d2 
-       << "    (fraction of linear: " << 1.0*d1/(d2*threadcount) << ")" << endl << endl;
-
-  Matrix<int64_t> t1(3, 3, 3), t2 = t1, t3(3,3,2), t4(3,2,3);
+  /*Matrix<int64_t> t1(3, 3, 3), t2 = t1, t3(3,3,2), t4(3,2,3);
 
   cout << "t1 == t2: " << (t1==t2?"yes":"no") << endl;
   cout << "t1 == t3: " << (t1==t3?"yes":"no") << endl;
@@ -182,6 +175,6 @@ int main(int argc, char const *argv[]) {
 
   x.tRand(3, 13, 7, 8);
   cout << x << endl;
-
+*/
   return 0;
 }
